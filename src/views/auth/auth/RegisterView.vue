f0257e65dad6aa989aa2362b3cd589e70c3fe2ca
edrian <edriantacmoy2005@gmail.com>
2025-04-05 13:43:33 +0800

    feat:added log in and registeration

--- src/views/auth/auth/RegisterView.vue
+++ src/views/auth/auth/RegisterView.vue
@@ -22,19 +22,26 @@ function onClick() {
 
       <v-main>
         <v-container>
-          <v-row>
-            <v-col cols="12" md="6" class="mx-auto"
-              ><v-card class="mx-auto" prepend-icon="mdi-account" subtitle="Log In" elevation="24">
+          <v-row class="d-flex justify-center">
+            <v-col cols="12" md="6"
+              ><v-card class="mx-auto" prepend-icon="mdi-login" subtitle="Register" elevation="24">
                 <template v-slot:title>
                   <span class="font-weight-black">BHC Tracker</span>
                 </template>
 
                 <v-card-text class="bg-surface-light pt-4">
                   <v-form fast-fail @submit.prevent>
+                    <v-text-field label="Firstname" variant="outlined"></v-text-field>
+                    <v-text-field label="Lastname" variant="outlined"></v-text-field>
                     <v-text-field label="Username" variant="outlined"></v-text-field>
 
                     <v-text-field
-                      label="Password"
+                      label="Password "
+                      type="password"
+                      variant="outlined"
+                    ></v-text-field>
+                    <v-text-field
+                      label="Password Confirmation"
                       type="password"
                       variant="outlined"
                     ></v-text-field>
@@ -43,12 +50,9 @@ function onClick() {
                   </v-form>
 
                   <v-divider class="my-5"></v-divider>
-
                   <h5 class="text-center">
-                    Don't have account?
-                    <RouterLink class="text-primary" to="/register"
-                      >Click here to Register</RouterLink
-                    >
+                    Already have account?
+                    <RouterLink class="text-primary" to="/">Click here to log in</RouterLink>
                   </h5>
                 </v-card-text>
               </v-card>
