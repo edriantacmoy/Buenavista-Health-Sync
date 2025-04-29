0c8b37424fe0f70e24b5fed02f51cff577ee9552
edrian <edriantacmoy2005@gmail.com>
2025-04-29 13:14:58 +0800

    fix : router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '@/views/auth/auth/LoginView.vue'
-import RegisterView from '@/views/auth/auth/RegisterView.vue'
-import DashboardView from '@/components/auth/DashboardView.vue'
-import RecordsView from '@/components/auth/RecordsView.vue'
+import LoginView from '@/views/auth/LoginView.vue'
+import RegisterView from '@/views/auth/RegisterView.vue'
+import DashboardView from '@/views/auth/DashboardView.vue'
+import RecordsView from '@/views/auth/RecordsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,7 +21,8 @@ const router = createRouter({
       path: '/dashboard',
       name: 'dashboard',
       component: DashboardView,
-    },{
+    },
+    {
       path: '/records',
       name: 'records',
       component: RecordsView,
